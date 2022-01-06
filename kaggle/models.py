e447e520ac929911895b7ee9905bab3875f61192
Adam Derylo <a.m.derylo@gmail.com>
2022-01-06 21:15:23 +0100

    moved to postgres

--- kaggle/models.py
+++ kaggle/models.py
@@ -16,40 +16,40 @@ class Cocktail(SQLModel, table=True):
 
 class Glassware(SQLModel, table=True):
     __tablename__ = "glassware"
-    id = Field(primary_key=True)
+    id: int = Field(primary_key=True)
     name: str
     photo: Optional[str]
 
 
 class Garnish(SQLModel, table=True):
     __tablename__ = "garnishes"
-    id = Field(primary_key=True)
+    id: int = Field(primary_key=True)
     name: str
     price: Optional[int]
 
 
 class Ingredient(SQLModel, table=True):
     __tablename__ = "ingredients"
-    id = Field(primary_key=True)
+    id: int = Field(primary_key=True)
     name: str
     price: Optional[int]
 
 
-class CocktailIngredient(SQLModel, table=True):
-    __tablename__ = "cocktail_ingredients"
-    __table_args__ = (UniqueConstraint("cocktail_id", "ingredient_id"),)
-    cocktail_id = int
-    ingredient_id = int
+# class CocktailIngredient(SQLModel, table=True):
+#     __tablename__ = "cocktail_ingredients"
+#     __table_args__ = (UniqueConstraint("cocktail_id", "ingredient_id"),)
+#     cocktail_id = int
+#     ingredient_id = int
 
 
-class CocktailGarnish(SQLModel, table=True):
-    __tablename__ = "cocktail_garnishes"
-    __table_args__ = (UniqueConstraint("cocktail_id", "garnish_id"),)
-    cocktail_id = int
-    garnish_id = int
+# class CocktailGarnish(SQLModel, table=True):
+#     __tablename__ = "cocktail_garnishes"
+#     __table_args__ = (UniqueConstraint("cocktail_id", "garnish_id"),)
+#     cocktail_id = int
+#     garnish_id = int
 
 
-class Substitute(SQLModel, table=True):
-    __tablename__ = "substitutes"
-    ingredient_id = int
-    substitute_id = int
+# class Substitute(SQLModel, table=True):
+#     __tablename__ = "substitutes"
+#     ingredient_id = int
+#     substitute_id = int
--- kaggle/pipeline.py
+++ kaggle/pipeline.py
@@ -1,6 +1,9 @@
 import os
 
 import pandas as pd
+from db import create_db_and_tables, engine
+from models import Glassware
+from sqlmodel import Session
 
 
 def dowload_dataset_from_kaggle() -> pd.DataFrame:
@@ -10,3 +13,9 @@ def dowload_dataset_from_kaggle() -> pd.DataFrame:
     df = pd.read_csv("all_drinks.csv")
     os.system("rm all_drinks.csv")
     return df
+
+
+def save_data(data):
+    create_db_and_tables()
+    with Session(engine) as session:
+        print(session.query(Glassware).get(1))
