48c73d0867a07024df090ec4eae3a841d8178da0
Adam Derylo <a.m.derylo@gmail.com>
2022-01-19 23:00:01 +0100

    working version to the speck

--- api/main.py
+++ api/main.py
@@ -24,53 +24,11 @@ def get_ingredients():
 
 @app.route("/cocktails")
 def get_cocktails():
-    ingredients = [13, 110, 78, 89, 57]  # ingredient_ids of Mojito
-    max_add = 1
-    total = len(ingredients) + max_add
-    result = []
-    query = """
-    WITH CTE1 AS (
-    SELECT cocktail_id, COUNT(*) all_ing
-    FROM cocktail_ingredients
-    GROUP BY cocktail_id
-    HAVING COUNT(*) < :total),
-     CTE2 AS (
-         SELECT cocktail_id, COUNT(*) listed_ingredients
-         FROM cocktail_ingredients
-         WHERE ingredient_id IN :essential
-         GROUP BY cocktail_id)
-    SELECT CTE2.cocktail_id
-    FROM CTE2
-            LEFT JOIN CTE1 ON CTE1.cocktail_id = CTE2.cocktail_id
-    WHERE all_ing - listed_ingredients <= :max_additional_ing
-    ORDER BY listed_ingredients desc;
-    """
-
-    for quantity in range(len(ingredients)):
-        combinations = itertools.combinations(set(ingredients), quantity + 1)
-        for subset in combinations:
-            with Session(engine) as session:
-                values = session.execute(
-                    query,
-                    {
-                        "total": total,
-                        "max_additional_ing": max_add,
-                        "essential": tuple(subset),
-                    },
-                )
-                result = result + [value["cocktail_id"] for value in values]
-
-    result = set(result)
-    result = [{"cocktail_id": id} for id in result]
-
-    return (json.dumps(result), 200, {"content_type": "application/json"})
-
-
-@app.get("/c/<int:max_additional>/<ingredients>")
-def get_cocktails_faster(max_additional=None, ingredients=None):
-    ingredients = list(ingredients.split(","))
-    # ingredients = [13, 110, 78, 89, 57]  # ingredient_ids of Mojito
-    max_add = max_additional
+    ids = request.args.get("ids")
+    count = request.args.get("count")
+    ids = [int(i) for i in ids.split(",")]
+    ingredients = ids
+    max_add = int(count)
     total = len(ingredients) + max_add
     result = []
     query = """
